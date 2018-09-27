e1e772b84a8ce04158b0cc2f9af4a08a2a3f8881
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2018-09-27 00:13:52 -0300

    Add Deck to Scene with basic draw function
    Closes k0uta/Little-Witch-CCG#2

---
+++ Assets/_LWCCG/Scripts/DeckModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LWCCG
+{
+    public class DeckModel : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Deck baseDeck;
+
+        private Deck deck;
+
+        [SerializeField]
+        private Text cardCountText;
+
+        [SerializeField]
+        private GameObject cardPrefab;
+
+        [SerializeField]
+        private Transform targetDock;
+
+        void UpdateCardCount()
+        {
+            cardCountText.text = string.Format("Count: {0}", deck.cards.Count);
+        }
+
+        void Start()
+        {
+            deck = Instantiate(baseDeck);
+            deck.OnDeckUpdate += UpdateCardCount;
+            deck.Shuffle();
+        }
+
+        public void Draw()
+        {
+            Card card = deck.Draw();
+            GameObject cardObject = Instantiate(cardPrefab);
+            cardObject.name = card.title;
+            cardObject.GetComponent<CardDisplay>().card = card;
+
+            cardObject.GetComponent<DraggableCard>().Dock(targetDock);
+            cardObject.transform.SetParent(targetDock);
+        }
+    }
+}
